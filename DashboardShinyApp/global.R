337bf4a115412c7debec0911e4cb5d060d92884d
Nagat Guled <nagat_g@hotmail.com>
2026-08-17 14:28:01 +0100

    Create global.R file

---
+++ DashboardShinyApp/global.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(bslib)
+library(shinyBS)
+library(patchwork)
+library(ggplot2)
+library(ggiraph)
+library(shinyWidgets)
+library(tidyverse)
+library(stringr)
+library(shinyjs)
+library(ggtext)
+library(gridtext)
+library(ggh4x)
+library(rsconnect)
+library(shinycssloaders)
+
+# current url:  https://h4e5yf-nagat0guled.shinyapps.io/winterpressuresdashboard/
+
+df <- read.csv("data/dummy_model_output.csv")
+
+source("R/labels.R")
+source("R/logos.R")
+
+uob_red <- "#a6192e"
+
+# map columns in dataset to labels for dashboard
